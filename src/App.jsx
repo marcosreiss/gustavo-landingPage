c91f196d80e718df90fa133d867aa72ad7b78d73
Marcos <m.vinicius.sr11@gmail.com>
2024-07-28 01:13:07 -0300

    subindo projeto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './css/style.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='background'></div>
+    <img className='logo' src='./src/assets/plano.png' alt='logo gustavo castroide' />
+    <div className='copy-1'>
+      <img src='./src/assets/SETA.png' />
+      <p>
+        Eu e minha equipe já atendemos mais de  <b>37 empresas em São Luís. </b> 
+        Posicionando suas marcas e vendendo pelo Instagram.
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
