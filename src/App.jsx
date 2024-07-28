7334312497e8733f2ea5959e11a49f2c9ea00c37
Marcos <m.vinicius.sr11@gmail.com>
2024-07-28 18:53:50 -0300

    adicionando mídia do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,26 @@ function App() {
     <>
     <div className='background'></div>
     <img className='logo' src='./src/assets/plano.png' alt='logo gustavo castroide' />
+
     <div className='copy-1'>
       <img src='./src/assets/SETA.png' />
       <p>
         Eu e minha equipe já atendemos mais de  <b>37 empresas em São Luís. </b> 
         Posicionando suas marcas e vendendo pelo Instagram.
+      </p>
+    </div>
+
+    <div className='copy-2'>
+      <p>
+        Nossa prioridade é fazer você ter retorno <b>(clientes comprando seu 
+          produto/serviço)</b> antes que nossos primeiros 30 dias de trabalho acabem
+      </p>
+      <img src='./src/assets/seta-direita.png' />
+    </div>
+    
+    <div className='copy-3'>
+      <p>
+          Para isso, desenvolvemos uma metodologia capaz de posicionar sua marca no mercado, mostrar o seu produto/serviço de forma profissional e captar clientes em grande escala.
       </p>
     </div>
     </>
