61675e27d98f8fbc1fe2bffbb71df2a1e864d0cc
Marcos <m.vinicius.sr11@gmail.com>
2024-07-28 20:11:42 -0300

    criação do component pilar1 e renomeação de arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
-import './css/style.css'
+import Header from './components/Header'
+import Pilar1 from './components/Pilar1'
 
 function App() {
 
   return (
     <>
     <div className='background'></div>
-    <img className='logo' src='./src/assets/plano.png' alt='logo gustavo castroide' />
-
-    <div className='copy-1'>
-      <img src='./src/assets/SETA.png' />
-      <p>
-        Eu e minha equipe já atendemos mais de  <b>37 empresas em São Luís. </b> 
-        Posicionando suas marcas e vendendo pelo Instagram.
-      </p>
-    </div>
-
-    <div className='copy-2'>
-      <p>
-        Nossa prioridade é fazer você ter retorno <b>(clientes comprando seu 
-          produto/serviço)</b> antes que nossos primeiros 30 dias de trabalho acabem
-      </p>
-      <img src='./src/assets/seta-direita.png' />
-    </div>
     
-    <div className='copy-3'>
-      <p>
-          Para isso, desenvolvemos uma metodologia capaz de posicionar sua marca no mercado, mostrar o seu produto/serviço de forma profissional e captar clientes em grande escala.
-      </p>
-    </div>
-    <section>
-      <img src='src\assets\pilares.png' className='pilares' />
-      
-    </section>
+    <Header />
+
+    <img src='src\assets\pilares.png' className='pilares' />
+
+    <Pilar1 />
     </>
   )
 }
---
+++ src/components/Pilar1.jsx
@@ -0,0 +1,12 @@
+import '../css/pilar1.css'
+
+export default function Pilar1(){
+
+
+
+    return(
+        <section>
+            
+        </section>
+    )
+}
