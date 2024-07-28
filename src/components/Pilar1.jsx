ec48a9255a57334d6629e0ebd0d693d11bda5af4
Marcos <m.vinicius.sr11@gmail.com>
2024-07-28 20:41:53 -0300

    primeira parte do pilar 2

--- src/components/Pilar1.jsx
+++ src/components/Pilar1.jsx
@@ -6,7 +6,12 @@ export default function Pilar1(){
 
     return(
         <section>
-            
+            <img src='src/assets/estruturacao.png' className='title' alt=''/>
+            <p className='copy'>
+                Antes de tudo, todos os pontos do seus perfil precisam corresponder a uma 
+                estrutura profissional de uma empresa no digital.
+            </p>
+            <img src='src/assets/phone.png' alt='' className='phone' />
         </section>
     )
 }
