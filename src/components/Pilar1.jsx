a770b505951802c72909522e9262c48a19bb1f9d
Marcos <m.vinicius.sr11@gmail.com>
2024-07-28 22:27:17 -0300

    Primeiro pilar finalizado

--- src/components/Pilar1.jsx
+++ src/components/Pilar1.jsx
@@ -11,7 +11,13 @@ export default function Pilar1(){
                 Antes de tudo, todos os pontos do seus perfil precisam corresponder a uma 
                 estrutura profissional de uma empresa no digital.
             </p>
-            <img src='src/assets/phone.png' alt='' className='phone' />
+            <img src='src\assets\phone\phone.png' alt='' className='phone' />
+            <img src='src\assets\phone\arroba.svg' alt='' className='arroba' />
+            <img src='src\assets\phone\perfil.svg' alt='' className='perfil' />
+            <img src='src\assets\phone\nome.svg' alt='' className='nome' />
+            <img src='src\assets\phone\biografia.svg' alt='' className='biografia' />
+            <img src='src\assets\phone\link.svg' alt='' className='link' />
+            <img src='src\assets\phone\destaques.png' alt='' className='destaques' />
         </section>
     )
 }
