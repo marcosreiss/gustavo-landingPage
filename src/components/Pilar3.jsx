315c426a56168a52648895e509dde5087d1826d9
Marcos <m.vinicius.sr11@gmail.com>
2024-07-29 10:59:04 -0300

    pilar 3 finalizado

--- src/components/Pilar3.jsx
+++ src/components/Pilar3.jsx
@@ -4,8 +4,23 @@ export default function Pilar3(){
 
 
     return(
-        <section>
-            
+        <section className='pilar3'>
+            <img src='src\assets\Pilar3\titulo.png' className='title' />
+            <p className='pilar3-p1'>
+                Depois de posicionar, precisamos ampliar o alcance, e o tráfego pago 
+                nos ajuda a entregar seu novo conteúdo profissional para um público 
+                qualificado e interessado no seu produto/serviço.
+            </p>
+
+            <p className='pilar3-p2'>
+                Atualmente, já desenvolvemos resultados inacreditáveis através desses pilares, 
+                e antes mesmo do nosso primeiro mês de trabalho encerrar.
+            </p>
+
+            <img src='src\assets\Pilar3\trafego-1.png' alt='' className='trafego trafego-1' />
+            <img src='src\assets\Pilar3\trafego-2.png' alt='' className='trafego trafego-2' />
+            <img src='src\assets\Pilar3\trafego-3.png' alt='' className='trafego trafego-3' />
+            <img src='src\assets\Pilar3\trafego-4.png' alt='' className='trafego trafego-4' />
         </section>
     )
 }
